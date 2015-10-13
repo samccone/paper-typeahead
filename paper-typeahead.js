2a0bc7fca078eaa29962a7fcfb492cb01f287c8b
Zecat <jullienfelix@gmail.com>
2015-10-13 10:03:14 +0200

    clean up old logic

--- paper-typeahead.js
+++ paper-typeahead.js
@@ -42,33 +42,6 @@
         computed: 'getFiltered(data.*, input, filterFn, maxResults)'
       }
     },
-    listeners: {
-      'blur': 'onBlur',
-    },
-    onBlur: function() {
-      this.hideResults = true;
-    },
-    onFocus: function() {
-      this.hideResults = false;
-    },
-    selected: function(e) {
-      this.input = e.detail.target;
-      this.fire('itemSelected', {selected: e.detail.target});
-      this.fire('blur');
-    },
-    checkKey: function(e) {
-      if (e.which === 40 && this.input.length) {
-        // has to be a nicer way to do this
-        document.querySelector('[tabindex="0"]').focus();
-        return;
-      }
-
-      if (e.which === 27) {
-        this.hideResults = true;
-      } else {
-        this.hideResults = false;
-      }
-    },
     getFiltered: function(data, input, filterFn, maxResults) {
       return filterFn.call(this, data.base, input)
         .slice(0, maxResults);
