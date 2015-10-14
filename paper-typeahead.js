6b558676f7e33648b07f2c2c0c7ff223374eef9e
Zecat <jullienfelix@gmail.com>
2015-10-14 13:48:56 +0200

    aerate code, start doc
    selectPrediction: function(selectedIndex)
    to
    selectPrediction: function(itemIndex)

--- paper-typeahead.js
+++ paper-typeahead.js
@@ -1,25 +1,33 @@
 (function() {
   'use strict';
+
   Polymer({
     is: 'paper-typeahead',
+
     behaviors: [
       Polymer.IronA11yKeysBehavior
     ],
+
     properties: {
       disabled: false,
+
       typeaheadDisabled: {
         type: Boolean,
         value: false,
       },
+
       value: {
         type: String,
         observer: '_valueChanged'
       },
+
       label: {
         type: String,
         value: ''
       },
+
       data: Array,
+
       filterFn: {
         type: Function,
         value: function() {
@@ -36,47 +44,57 @@
           };
         }
       },
+
       maxResults: {
         type: Number,
         value: 10
       },
+
       elevation: {
         type: Number,
         value: 1
       },
+
       filteredItems: {
         type: Array,
         computed: '_getFiltered(data.*, value, filterFn, maxResults, typeaheadDisabled)'
       },
+
       keyEventTarget: {
         type: Object,
         value: function() {
           return this;
         }
       },
+
       // private because we don't want the user to set it true if there is no results
       _hideResults: {
         type: Boolean,
         value: false
       },
     },
+
     keyBindings: {
       'up': '_upPressed',
       'down': '_downPressed',
       'esc': '_escPressed',
       'enter': '_enterPressed'
     },
+
     listeners: {
       'iron-activate': '_itemPressed'
     },
+
     _itemPressed: function(e) {
       this.selectPrediction(e.detail.selected);
     },
+
     _upPressed: function() {
       if (!this._hideResults) {
         this.$.predictions.selectPrevious();
       }
     },
+
     _downPressed: function() {
       if (!this._hideResults) {
         this.$.predictions.selectNext();
@@ -86,38 +104,46 @@
         this._hideResults = false;
       }
     },
+
     _escPressed: function() {
       this.$.predictions.selected = 0;
       this._hideResults = true;
     },
+
     _enterPressed: function() {
       this.selectPrediction(this.$.predictions.selected);
     },
+
     _getFiltered: function(data, value, filterFn, maxResults, typeaheadDisabled) {
       if (typeaheadDisabled) { return []; }
       return filterFn.call(this, data.base, value)
         .slice(0, maxResults);
     },
+
     _valueChanged: function() {
       this.$.predictions.selected = 0;
       this._hideResults = this.filteredItems.length ? false : true;
     },
+
     _blur: function() {
       // paper-item gain focus on-tap so _blur is called too early
       // this.closePredictions();
     },
+
     /**
-     * Select a prediction by index then hide and reset the predictions.
+     * Select a prediction by index then close the predictions.
+     *
+     * @param {Number} itemIndex The index of the item to select
      */
-    selectPrediction: function(selectedIndex) {
-      this.value = this.filteredItems[selectedIndex];
-      this.$.predictions.selected = 0;
-      this._hideResults = true;
+    selectPrediction: function(itemIndex) {
+      this.value = this.filteredItems[itemIndex];
+      this.closePredictions();
     },
+
     /**
      * Manually display the predictions if the results list is not empty.
      *
-     * @return {boolean} True if the predictions are displayed.
+     * @return {Boolean} True if the predictions are displayed.
      */
     tryDisplayPredictions: function() {
       if (this._hideResults && this.filteredItems.length) {
@@ -125,19 +151,21 @@
       }
       return !_hideResults;
     },
+
     /**
      * Manually hide the predictions.
      */
     hidePredictions: function() {
       this._hideResults = true;
     },
+
     /**
      * Manually hide the predictions and reset selected.
      */
     closePredictions: function() {
       this._hideResults = true;
       this.selected = 0;
-    },
+    }
+
   });
 })();
-
