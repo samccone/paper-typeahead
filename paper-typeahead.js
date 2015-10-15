516044dff165b85b3c4aa2b2285ab78732964cfe
Zecat <jullienfelix@gmail.com>
2015-10-15 10:15:56 +0200

    delete line space

--- paper-typeahead.js
+++ paper-typeahead.js
@@ -1,72 +1,58 @@
 (function() {
   'use strict';
-
   Polymer({
     is: 'paper-typeahead',
-
     behaviors: [
       Polymer.IronA11yKeysBehavior,
       Polymer.IronSelectableBehavior,
       Polymer.IronTypeaheadBehavior
     ],
-
     properties: {
-
       disabled: {
         type: Boolean,
         value: false
       },
-
       value: {
         type: String,
         observer: '_valueChanged'
       },
-
       label: {
         type: String,
         value: ''
       },
-
       elevation: {
         type: Number,
         value: 1
       },
-
       keyEventTarget: {
         type: Object,
         value: function() {
           return this;
         }
       },
-
       // private because we don't want the user to set it true if there is no results
       _hideResults: {
         type: Boolean,
         value: false
       },
     },
-
     keyBindings: {
       'up': '_upPressed',
       'down': '_downPressed',
       'esc': '_escPressed',
       'enter': '_enterPressed'
     },
-
     listeners: {
       'iron-activate': '_itemPressed'
     },
-
     _itemPressed: function(e) {
       this.selectResult(e.detail.selected);
     },
-
     _upPressed: function() {
       if (!this._hideResults) {
         this.selectPrevious();
       }
     },
-
     _downPressed: function() {
       if (!this._hideResults) {
         this.selectNext();
@@ -76,34 +62,27 @@
         this._hideResults = false;
       }
     },
-
     _escPressed: function() {
       this.selected = 0;
       this._hideResults = true;
     },
-
     _enterPressed: function() {
       this.selectResult(this.selected);
     },
-
     _valueChanged: function() {
       this.selected = 0;
       this._hideResults = this.filteredItems.length ? false : true;
     },
-
     _blur: function() {
       // paper-item gain focus on-tap so _blur is called too early
       // this.closeResults();
     },
-
     _mouseoverItem: function(e) {
       this.select(this.indexOf(e.target));
     },
-
     get items() {
       return Array.from(this.querySelectorAll('.selectable'));
     },
-
     /**
      * Select a Result in the filteredItems array by index then close the results.
      *
@@ -113,7 +92,6 @@
       this.value = this.filteredItems[itemIndex];
       this.closeResults();
     },
-
     /**
      * Manually display the results if the filteredItems array is not empty.
      *
@@ -125,14 +103,12 @@
       }
       return !_hideResults;
     },
-
     /**
      * Manually hide the results.
      */
     hideResults: function() {
       this._hideResults = true;
     },
-
     /**
      * Manually hide the results and reset selected item.
      */
@@ -140,6 +116,5 @@
       this._hideResults = true;
       this.selected = 0;
     }
-
   });
 })();
