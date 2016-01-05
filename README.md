df015d382889f1590c021152fcfe8d32b0b59406
Sam Saccone <samccone@gmail.com>
2016-01-05 15:24:23 -0800

    Add closure docs in methods

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ function(data, input) { return data; }
 * [x] Add tab / enter result selection
 * [ ] Add conditional empty list template
 * [x] Add better style interface
-* [ ] jsdoc all the methods
+* [x] jsdoc all the methods
 * [x] extract js to external file
 * [ ] add tests
 * [ ] Any other ideas welcome 🎷
--- paper-typeahead.js
+++ paper-typeahead.js
@@ -1,7 +1,9 @@
 (function() {
   'use strict';
+
   Polymer({
     is: 'paper-typeahead',
+
     behaviors: [
       Polymer.IronA11yKeysBehavior,
       Polymer.IronSelectableBehavior,
@@ -9,49 +11,59 @@
       Polymer.IronControlState,
       Polymer.IronFormElementBehavior
     ],
+
     properties: {
       arrowsUpdateInput: {
         type: Boolean,
         value: false
       },
+
       showEmptyResults: {
         type: Boolean,
         value: false
       },
+
       typedValue: {
         type: String,
         observer: '_typedValueChanged',
         value: '',
         notify: true
       },
+
       elevation: {
         type: Number,
         value: 1
       },
+
       keyEventTarget: {
         type: Object,
         value: function() {
           return this;
         }
       },
+
       typeaheadDisabled: {
         type: Boolean,
         value: false
       },
+
       data: {
         type: Array,
         value: function() { return []; }
       },
+
       maxResults: {
         type: Number,
         value: 10
       },
+
       filteredItems: {
         type: Array,
         computed: '_getFiltered(data.*, typedValue, filterFn, maxResults,' +
         'typeaheadDisabled)',
         notify: true
       },
+
       filterFn: {
         type: Function,
         value: function() {
@@ -68,6 +80,7 @@
           };
         }
       },
+
       // private because we don't want the user to
       // set it true if there is no results
       _hideResults: {
@@ -75,23 +88,34 @@
         value: true
       },
     },
+
     keyBindings: {
       'up': '_upPressed',
       'down': '_downPressed',
       'esc': '_escPressed',
       'enter': '_enterPressed'
     },
+
     listeners: {
       'iron-activate': '_itemPressed',
       'focus': '_onFocus',
       'blur': '_onBlur',
     },
+
+    /**
+     * @private
+     * @param {Event} e
+     */
     _itemPressed: function(e) {
       // if pressed item is not paper-input-container
       if (e.detail.selected) {
         this.selectResult(e.detail.selected - 1);
       }
     },
+
+    /**
+     * @private
+     */
     _upPressed: function() {
       if (!this._hideResults) {
         this.selectPrevious();
@@ -99,6 +123,10 @@
           this.filteredItems[this.selected - 1] : this.typedValue;
       }
     },
+
+    /**
+     * @private
+     */
     _downPressed: function() {
       if (!this._hideResults) {
         this.selectNext();
@@ -110,10 +138,18 @@
         this._hideResults = false;
       }
     },
+
+    /**
+     * @private
+     */
     _escPressed: function() {
       this.selected = 0;
       this._hideResults = true;
     },
+
+    /**
+     * @private
+     */
     _enterPressed: function() {
       // -1 since paper-input-container is part of
       // selectable array, index is shifted
@@ -121,6 +157,10 @@
         this.selectResult(this.selected - 1);
       }
     },
+
+    /**
+     * @private
+     */
     _typedValueChanged: function() {
       var hasItems = this.filteredItems && this.filteredItems.length;
       if (hasItems) {
@@ -129,26 +169,41 @@
 
       this._hideResults = !hasItems;
     },
+
+    /**
+     * @private
+     */
     _mouseenterItem: function(e) {
       this.select(this.indexOf(e.target));
     },
+
+    /**
+     * @private
+     */
     _mouseleaveItems: function() {
       this.selected = 0;
     },
+
+    /**
+     * @private
+     * @return {Array}
+     */
     _getFiltered: function(
       data, typedValue, filterFn, maxResults, typeaheadDisabled) {
       if (typeaheadDisabled) { return []; }
       return filterFn.call(this, data.base, typedValue)
         .slice(0, maxResults);
     },
+
     get items() {
       return Array.from(Polymer.dom(this.root).querySelectorAll('.selectable'));
     },
+
     /**
      * Select a Result in the filteredItems array by index then
      * close the results.
      *
-     * @param {Number} itemIndex The index of the item to select
+     * @param {!number} itemIndex The index of the item to select
      */
     selectResult: function(itemIndex) {
       this.typedValue = this.value = this.filteredItems[itemIndex];
@@ -156,11 +211,12 @@
       this.fire('selected', {target: this.value});
       this.closeResults();
     },
+
     /**
      * Manually display the results if the filteredItems array is not empty.
      *
-     * @return {Boolean} True if the results are displayed.
-     */
+     * @return {boolean} True if the results are displayed.
+    */
     tryDisplayResults: function() {
       var items = this.filteredItems;
 
@@ -170,12 +226,14 @@
 
       return !this._hideResults;
     },
+
     /**
      * Manually hide the results.
      */
     hideResults: function() {
       this._hideResults = true;
     },
+
     /**
      * Manually hide the results and reset selected item.
      */
@@ -183,15 +241,26 @@
       this._hideResults = true;
       this.selected = 0;
     },
+
     /**
      * Stop the _onBlur event from firing when scrollbar is clicked.
+     *
+     * @param {!Event} e
      */
-    _mouseDownItems: function(event) {
-      event.preventDefault();
+    _mouseDownItems: function(e) {
+      e.preventDefault();
     },
+
+    /**
+     * @private
+     */
     _onFocus: function() {
       this.tryDisplayResults();
     },
+
+    /**
+     * @private
+     */
     _onBlur: function() {
       this.closeResults();
     },
